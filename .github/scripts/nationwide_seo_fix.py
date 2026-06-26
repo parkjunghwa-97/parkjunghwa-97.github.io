b0e8998ae80f2344bc9fe0912537260d942c6a0d
parkjunghwa-97 <giftclean0529@gmail.com>
2026-06-26 15:21:26 +0900

    Fix checklist card insertion

--- .github/scripts/nationwide_seo_fix.py
+++ .github/scripts/nationwide_seo_fix.py
@@ -6,12 +6,13 @@ s = p.read_text(encoding='utf-8')
 
 card = '<div class="movein-check"><b>\uc2e0\ucd95 \uc785\uc8fc \uc804\ud6c4 \ud655\uc778\ud558\uba74 \uc88b\uc740 \uccb4\ud06c\ub9ac\uc2a4\ud2b8</b><p>\uc815\ubc00 \uc810\uac80\uc740 \uc544\ub2c8\uc9c0\ub9cc, \uc785\uc8fc\uccad\uc18c \uacfc\uc815\uc5d0\uc11c \ub208\uc5d0 \ub744\ub294 \ub9c8\uac10 \uc0c1\ud0dc\uc640 \uc0dd\ud65c \uccb4\ud06c \ud3ec\uc778\ud2b8\ub97c \ud568\uaed8 \ud655\uc778\ud569\ub2c8\ub2e4.</p><div><span>\ucc3d\ud2c0\xb7\uc0f7\uc2dc</span><span>\uc2f1\ud06c\ub300 \ud558\ubd80</span><span>\uc695\uc2e4 \ubc30\uc218</span><span>\ubb38\ud2c0\xb7\ubab0\ub529</span><span>\uc218\ub0a9\uc7a5 \ub0b4\ubd80</span><span>\ubc14\ub2e5 \ucc0d\ud798</span></div><a href="/cases/new-apartment-checklist.html">30\uac00\uc9c0 \uccb4\ud06c\ub9ac\uc2a4\ud2b8 \ubcf4\uae30</a></div>'
 
-s = re.sub(r'\s*<div class="movein-check".*?</div>\s*', '\n', s, flags=re.S)
+s = re.sub(r'\s*<div class="movein-check".*?</a></div>\s*', '\n', s, flags=re.S)
+s = re.sub(r'\s*<a href="/cases/new-apartment-checklist.html">.*?</a></div>\s*', '\n', s, flags=re.S)
 anchor = '<div class="service-area" aria-label="서비스 가능 지역 안내">'
 if anchor in s:
     s = s.replace(anchor, card + '\n' + anchor, 1)
 
-faq = '<div class="faq-card"><b>\uc785\uc8fc\uccad\uc18c\ud560 \ub54c \ud558\uc790\ub3c4 \uac19\uc774 \ud655\uc778\ud574\uc8fc\uc2dc\ub098\uc694?</b><p>\uc815\ubc00 \ud558\uc790\uc9c4\ub2e8\uc740 \uc544\ub2c8\uc9c0\ub9cc, \uccad\uc18c \uacfc\uc815\uc5d0\uc11c \ub208\uc5d0 \ub744\ub294 \ub9c8\uac10 \uc0c1\ud0dc\ub098 \uc0dd\ud65c \ud558\uc790 \uac00\ub2a5 \ubd80\ubd84\uc740 \ud568\uaed8 \ud655\uc778\ud574\ub4dc\ub9bd\ub2c8\ub2e4.</p></div>'
+faq = '<div class="faq-card"><b>\uc785\uc8fc\uccad\uc18c\ud560 \ub54c \ud558\uc790\ub3c4 \uac19\uc774 \ud655\uc778\ud574\uc8fc\uc2dc\ub098요?</b><p>\uc815\ubc00 \ud558\uc790\uc9c4\ub2e8\uc740 \uc544\ub2c8\uc9c0\ub9cc, \uccad\uc18c \uacfc\uc815\uc5d0\uc11c \ub208\uc5d0 \ub744\ub294 \ub9c8\uac10 \uc0c1\ud0dc\ub098 \uc0dd\ud65c \ud558\uc790 \uac00\ub2a5 \ubd80\ubd84\uc740 \ud568\uaed8 \ud655\uc778\ud574\ub4dc\ub9bd\ub2c8\ub2e4.</p></div>'
 s = re.sub(r'\s*<div class="faq-card"><b>.*?\ud558\uc790.*?</div>', '', s, flags=re.S)
 if '<div class="faq-grid">' in s and faq not in s:
     s = s.replace('<div class="faq-grid">', '<div class="faq-grid">\n        ' + faq, 1)
