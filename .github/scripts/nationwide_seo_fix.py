fa402daff62162909c8db0dd85fd167945d00832
parkjunghwa-97 <giftclean0529@gmail.com>
2026-06-26 10:08:24 +0900

    Restore FAQ intro text

--- .github/scripts/nationwide_seo_fix.py
+++ .github/scripts/nationwide_seo_fix.py
@@ -6,7 +6,9 @@ old = '<button class="nav-btn" onclick="showPage(\'contact\')">상담문의</but
 new = '<button class="nav-btn" onclick="location.href=\'partner.html\'">파트너모집</button>\n      ' + old
 if 'partner.html' not in s:
     s = s.replace(old, new, 1)
+intro = '\n        <p class="more-info-intro">예약금, 추가비용, A/S처럼<br>상담 전에 궁금한 부분을 미리 확인하기</p>'
 s = re.sub(r'\n\s*<p class="more-info-intro">.*?</p>', '', s, count=1, flags=re.S)
+s = s.replace('        <summary>작업 전 안내사항 보기</summary>', '        <summary>작업 전 안내사항 보기</summary>' + intro, 1)
 fixes = {
     '작업 범위는 상담 기준으로 진행돼요 내': '작업 범위 내',
     '작업 범위는 상담 기준으로 진행돼요에 대한': '작업 범위에 대한',
