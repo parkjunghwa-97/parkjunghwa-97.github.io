7b5c3b68d779e315cc82d0ce6bc201c75da0283a
parkjunghwa-97 <giftclean0529@gmail.com>
2026-06-26 10:06:35 +0900

    Update FAQ intro copy

--- .github/scripts/nationwide_seo_fix.py
+++ .github/scripts/nationwide_seo_fix.py
@@ -5,6 +5,7 @@ old = '<button class="nav-btn" onclick="showPage(\'contact\')">상담문의</but
 new = '<button class="nav-btn" onclick="location.href=\'partner.html\'">파트너모집</button>\n      ' + old
 if 'partner.html' not in s:
     s = s.replace(old, new, 1)
+s = s.replace('예약금, 추가비용, A/S처럼 상담 전에 궁금한 부분을 미리 확인하실 수 있어요.', '예약금, 추가비용, A/S처럼<br>상담 전에 궁금한 부분을 미리 확인하기')
 fixes = {
     '작업 범위는 상담 기준으로 진행돼요 내': '작업 범위 내',
     '작업 범위는 상담 기준으로 진행돼요에 대한': '작업 범위에 대한',
